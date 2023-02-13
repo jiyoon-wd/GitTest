697678f96e740b980c0d4252c844a40c591a358c
jiyoon-wd <jylpo121@gmail.com>
2023-02-12 19:00:12 -0500

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Jiyoon on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
