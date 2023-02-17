cc0015f53f39a75d8f2cf59fa578d858a369faa6
jiyoon-wd <jylpo121@gmail.com>
2023-02-16 19:32:03 -0500

    Swift Demo example.

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var items:[String] = []
+    @State var currentText = " "
     var body: some View {
-        VStack {
-            Image(systemName: "minus")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack {//vertically, HStack - horizontally ZStack- on top of stuffs
+            Text("Grocery  List")
+                .font(.system(.title, design: .rounded, weight: .bold))
+                .padding(50)
+                .background(Color.blue)
+            List (items, id: \.self){
+                item in
+                Text(item)
+            }
+            HStack {
+                TextField("Add item here", text: $currentText)
+                Button(
+                    action:{addItem(item: currentText)}, label: { Image(systemName: "square.fill")})
+            }
+            .padding(25)
+            .background(.cyan)
+            .cornerRadius(15)
+            .padding(15)
+            }
         }
-        .padding()
+    func addItem(item:String) {
+        items.append(item)
     }
-}
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
